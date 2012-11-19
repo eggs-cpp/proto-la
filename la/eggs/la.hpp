93a8e306f0aece5e62f16ed88641970266475e53
K-ballo <k@fusionfenix.com>
2012-11-19 18:05:33 -0300

    Added include-all header
    Signed-off-by: K-ballo <k@fusionfenix.com>

---
+++ la/eggs/la.hpp
@@ -0,0 +1,22 @@
+/**
+ * Eggs.LA <eggs/la.hpp>
+ * 
+ * Copyright Agustín Bergé, Fusion Fenix 2012
+ * 
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * 
+ * Library home page: http://github.com/eggs-cpp/eggs-la
+ */
+
+#ifndef EGGS_LA_HPP
+#define EGGS_LA_HPP
+
+// #include <eggs/la/matrix.hpp>
+// #include <eggs/la/matrix_ref.hpp>
+#include <eggs/la/matrix_traits.hpp>
+#include <eggs/la/vector.hpp>
+#include <eggs/la/vector_ref.hpp>
+#include <eggs/la/vector_traits.hpp>
+
+#endif /*EGGS_LA_HPP*/
