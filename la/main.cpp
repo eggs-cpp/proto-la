58de1e26b5f78eee7a39a4f36f23adf924db6479
K-ballo <k@fusionfenix.com>
2012-11-24 18:05:29 -0300

    reworked time tests
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- la/main.cpp
+++ la/main.cpp
@@ -9,6 +9,7 @@
  * Library home page: http://github.com/eggs-cpp/eggs-la
  */
 
+#include <eggs/la/matrix.hpp>
 #include <eggs/la/vector.hpp>
 
 #include <boost/proto/proto.hpp>
@@ -27,60 +28,54 @@ static const int cloops = 1;
 
 typedef eggs::la::vector< int, 3 > ivec3;
 
+ivec3 eager_time( ivec3 const& p, ivec3 const& q, ivec3 const& r )
+{
+    ivec3 ret;
+
+    boost::timer tim;
+    tim.restart();
+    for( int i = 0; i < cloops; ++i )
+    {
+        ret += ivec3( ivec3( p * 3 ) + ivec3( r - q ) );
+    }
+    double d = tim.elapsed();
+    
+    std::cout << d << 's' << '\n';
+    return ret;
+}
+
+ivec3 lazy_time( ivec3 const& p, ivec3 const& q, ivec3 const& r )
+{
+    ivec3 ret;
+
+    boost::timer tim;
+    tim.restart();
+    for( int i = 0; i < cloops; ++i )
+    {
+        ret += ( p * 3 + ( r - q ) );
+    }
+    double d = tim.elapsed();
+    
+    std::cout << d << 's' << '\n';
+    return ret;
+}
+
 ivec3 unrolled_time( ivec3 const& p, ivec3 const& q, ivec3 const& r )
 {
     ivec3 ret;
 
     boost::timer tim;
     tim.restart();
-    for (int i = 0; i < cloops; ++i)
+    for( int i = 0; i < cloops; ++i )
     {
         ret[0] += ( p[0] * 3 + ( r[0] - q[0] ) );
         ret[1] += ( p[1] * 3 + ( r[1] - q[1] ) );
         ret[2] += ( p[2] * 3 + ( r[2] - q[2] ) );
     }
     double d = tim.elapsed();
-
-    std::cout << " time: " << d << std::endl;
-    return ret;
-}
-
-ivec3 proto_time( ivec3 const& p, ivec3 const& q, ivec3 const& r )
-{
-    ivec3 ret;
-
-    boost::timer tim;
-    tim.restart();
-    for (int i = 0; i < cloops; ++i)
-    {
-        ret += ( p * 3 + ( r - q ) );
-    }
-    double d = tim.elapsed();
-
-    std::cout << " time: " << d << std::endl;
-    return ret;
-}
-
-template< typename Expr >
-ivec3 do_partial_proto_time( Expr& expr )
-{
-    ivec3 ret;
-
-    boost::timer tim;
-    tim.restart();
-    for (int i = 0; i < cloops; ++i)
-    {
-        ret += expr;
-    }
-    double d = tim.elapsed();
     
-    std::cout << " time: " << d << std::endl;
+    std::cout << d << 's' << '\n';
     return ret;
-}
-
-ivec3 partial_proto_time( ivec3 const& p, ivec3 const& q, ivec3 const& r )
-{
-    return do_partial_proto_time( p * 3 + ( r - q ) );
 }
 
 int main( int argc, char** argv )
@@ -96,29 +91,29 @@ int main( int argc, char** argv )
         >> q[0] >> q[1] >> q[2]
         >> r[0] >> r[1] >> r[2]
         ;
+    std::cout << "Eager: ";
+    std::cout << eager_time( p, q, r );
+    std::cout << "\n" << std::endl;
+            
+    input.seekg( 0, std::ios::beg );
+    input
+        >> p[0] >> p[1] >> p[2]
+        >> q[0] >> q[1] >> q[2]
+        >> r[0] >> r[1] >> r[2]
+        ;
+    std::cout << "Lazy: ";
+    std::cout << lazy_time( p, q, r );
+    std::cout << "\n" << std::endl;
+            
+    input.seekg( 0, std::ios::beg );
+    input
+        >> p[0] >> p[1] >> p[2]
+        >> q[0] >> q[1] >> q[2]
+        >> r[0] >> r[1] >> r[2]
+        ;
     std::cout << "Unrolled: ";
-    print( std::cout, unrolled_time( p, q, r ) );
-    std::cout << "\n\n";
-            
-    input.seekg( 0, std::ios::beg );
-    input
-        >> p[0] >> p[1] >> p[2]
-        >> q[0] >> q[1] >> q[2]
-        >> r[0] >> r[1] >> r[2]
-        ;
-    std::cout << "Proto: ";
-    print( std::cout, proto_time( p, q, r ) );
-    std::cout << "\n\n";
-            
-    input.seekg( 0, std::ios::beg );
-    input
-        >> p[0] >> p[1] >> p[2]
-        >> q[0] >> q[1] >> q[2]
-        >> r[0] >> r[1] >> r[2]
-        ;
-    std::cout << "Partial Proto: ";
-    print( std::cout, partial_proto_time( p, q, r ) );
-    std::cout << "\n\n";
+    std::cout << unrolled_time( p, q, r );
+    std::cout << "\n" << std::endl;
 
     int breakpoint = 3;
     std::cin >> breakpoint;
